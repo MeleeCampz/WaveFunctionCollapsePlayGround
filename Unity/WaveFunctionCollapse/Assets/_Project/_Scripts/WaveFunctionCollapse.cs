75c66c588ade2cca92ccb07fcddab7d78d236600
Tobias <t.pretzl@gmail.com>
2022-10-06 18:43:41 +0200

    - Basic empty unity project

---
+++ Unity/WaveFunctionCollapse/Assets/_Project/_Scripts/WaveFunctionCollapse.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveFunctionCollapse
+{
+    //TODO Implement me!
+    public void Collapse()
+    {
+
+    }
+}
